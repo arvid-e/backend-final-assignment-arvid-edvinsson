d5ee8415ede81e8d1ac78a12ec01fbc8d45004b7
arvid-e <arvid.edvinsson@gmail.com>
2025-07-23 09:43:13 +0900

    Add signup path and template

--- twittercopy/urls.py
+++ twittercopy/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from users.views import SignUpView 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),
+
     path('users/', include('users.urls')),
     path('tweets/', include('tweets.urls')),  
     path('followers/', include('followers.urls')),
--- users/views.py
+++ users/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import CustomUser
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import CustomUserCreationForm
 
 
 def profile_view(request, username):
     user = get_object_or_404(CustomUser, username=username)
     context = {'user': user}
     return render(request, 'users/profile.html', context)
+
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
